68936dc6bd3cd151ffaa9976b8f5a60ce3b51129
Kenneth Dy <kennethndy@yahoo.com>
2021-04-01 23:08:36 -0700

    boilerplate backend code and finished model

---
+++ models/Workout.js
@@ -0,0 +1,24 @@
+const { model, Schema } = require('mongoose')
+
+// const exercise = model('Excercise', new Schema({
+//   type: String,
+//   name: String,
+//   duration: Number,
+//   weight: Number,
+//   reps: Number,
+//   sets: Number,
+//   distance: Number
+// }))
+
+module.exports = model('Workout', new Schema({
+  date: Date,
+  exercises: [{
+    type: String,
+    name: String,
+    duration: Number,
+    weight: Number,
+    reps: Number,
+    sets: Number,
+    distance: Number
+  }]
+}))
