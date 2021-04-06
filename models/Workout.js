9715d2e7f95c544ba2361fc71b20177e67909590
Kenneth Dy <kennethndy@yahoo.com>
2021-04-06 10:36:05 -0700

    recreated models and routes

--- models/Workout.js
+++ models/Workout.js
@@ -10,8 +10,8 @@ const { model, Schema } = require('mongoose')
 //   distance: Number
 // }))
 
-module.exports = model('Workout', new Schema({
-  date: Date,
+const WorkoutSchema = new Schema({
+  day: Date,
   exercises: [{
     type: String,
     name: String,
@@ -22,5 +22,34 @@ module.exports = model('Workout', new Schema({
     distance: Number
   }],
 },
-  { typeKey: '$type' }
-  ))
+  { typeKey: '$type', toJSON: { virtuals: true }, toObject: { virtuals: true } }
+)
+
+WorkoutSchema.virtual('totalDuration').get(function () {
+  let total = 0
+
+  this.exercises.forEach(exercise => {
+    total += exercise.duration
+  })
+  return total
+})
+
+WorkoutSchema.virtual('totalWeights').get(function () {
+  let total = 0
+
+  this.exercises.forEach(exercise => {
+    total += exercise.weight
+  })
+  return total
+})
+
+WorkoutSchema.virtual('totalWorkouts').get(function () {
+  let total = []
+
+  this.exercises.forEach(exercise => {
+    total.push(exercise.name)
+  })
+  return total
+})
+
+module.exports = model('Workout', WorkoutSchema)
--- routes/workoutRoutes.js
+++ routes/workoutRoutes.js
@@ -3,25 +3,46 @@ const { Workout } = require('../models')
 
 router.get('/workouts', (req, res) => Workout.find()
   .then(workouts => res.json(workouts))
-  .catch(err => console.log(err)))
+  .catch(err => console.log(err))
+)
 
-router.post('/workouts', (req, res) => Workout.create(req.body)
-  .then(workout => res.json(workout))
-  .catch(err => console.log(err)))
+router.post('/workouts', (req, res) => {
+  
+  Workout.create({
+    day: new Date().setDate(new Date().getDate() - 0),
+    exercises: []
+  })
+    .then(workout => res.json(workout))
+    .catch(err => console.log(err))
+})
 
-router.put('/workouts/:id', (req, res) => Workout.findById(req.params.id)
+router.put('/workouts/:id', (req, res) => { 
+  Workout.findByIdAndUpdate(req.params.id, {
+    $push: {
+      exercises: req.body
+    }
+  })
   .then(workout => {
-    let newWorkout = workout.exercises
-    newWorkout.push(req.body)
-    Workout.findByIdAndUpdate(req.params.id, newWorkout)
-      .then(updatedWorkout => res.json(updatedWorkout))
-      .catch(err => console.log(err))
+    res.json(workout)
   })
   .catch(err => console.log(err))
-  )
+})
 
 router.get('/workouts/range', (req, res) => Workout.find()
-  .then(workouts => res.json(workouts))
-  .catch(err => console.log(err)))
+  .then(workouts => {
+    let totalWorkouts = workouts.map(exe => {
+      return {
+        exercises: [{
+          duration: exe.totalDuration,
+          weight: exe.totalWeights,
+          name: exe.totalWorkouts
+        }]
+      }
+    })
+    res.json(totalWorkouts)
+    // res.json(workouts)
+  })
+  .catch(err => console.log(err))
+)
 
 module.exports = router
